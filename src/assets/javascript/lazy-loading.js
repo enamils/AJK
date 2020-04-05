ba8238b80dc5107395338a8c83549c78c5c726ec
Slimane GASSAMA <sgassama@bouyguestelecom.fr>
2020-04-05 17:11:48 +0200

    feat: init repertory for webpack project

---
+++ src/assets/javascript/lazy-loading.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('.lazy').Lazy({
+        // your configuration goes here
+        effect: "fadeIn",
+        effectTime: 2000,
+        threshold: 0,
+        onError: function (element) {
+            console.log('error loading ' + element.data('src'));
+        }
+    });
+})

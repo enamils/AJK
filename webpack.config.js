53f3caa6607ac215fb1a4d93c387b78657365732
Slimane GASSAMA <sgassama@bouyguestelecom.fr>
2020-04-06 18:40:42 +0200

    feat: add css sass icons images build

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const webpack = require("webpack");
+const glob = require("glob");
 const WebpackBar = require('webpackbar');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const fs = require("fs");
 const yaml = require("js-yaml");
@@ -17,6 +19,10 @@ module.exports = {
   entry: {
     vendor: PATHS.vendor,
     app: PATHS.javascript,
+    bootstrapFontAwesome: PATHS.stylesheet.vendor,
+    style: PATHS.stylesheet.css,
+    images: glob.sync(PATHS.assets.images),
+    fonts: glob.sync(PATHS.assets.fonts)
   },
   output: {
     path: path.resolve(__dirname, PATHS.dist),
@@ -27,6 +33,9 @@ module.exports = {
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css'
     })
   ],
 
@@ -36,7 +45,47 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "babel-loader"
-      }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+          },
+          {
+            loader: 'resolve-url-loader'
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2|svg)$/,
+        exclude: [
+          /images/,
+        ],
+        loader: 'file-loader',
+        options: {
+          name: 'fonts/[name].[ext]'
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|svg|JPG|PNG|ico)$/,
+        exclude: [
+          /fonts/,
+        ],
+        use: 'file-loader?name=/images/[folder]/[name].[ext]',
+      },
     ]
   },
   devServer: {
